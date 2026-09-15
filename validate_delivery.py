c7ab658e12a49e0593b8c727b721a0b96b7b8dec
Doaa Alaaelden <doaaalaa237@gmail.com>
2026-09-15 22:37:59 +0300

    edit make report function

--- validate_delivery.py
+++ validate_delivery.py
@@ -21,9 +21,18 @@ def validate_deliveries(file_path: str, max_weight: float = 10) -> list[Delivery
 
 def make_report(invalid_deliveries, reasons, max_weight=10):
     with open("invalid_deliveries_report.txt", "w") as report_file:
+        report_file.write(
+            f"========================================\n"
+            f"INVALID DELIVERY REPORT\n"
+            f"========================================\n"
+            f"Vehicle capacity: {max_weight:g} kg\n"
+            f"Invalid deliveries: {len(invalid_deliveries)}\n\n"
+        )
         for delivery, reason in zip(invalid_deliveries, reasons):
-            report_file.write(f"========================================\nINVALID DELIVERY REPORT\n========================================\nVehicle capacity: {max_weight:g} kg\nInvalid deliveries:{len(invalid_deliveries)}\n\n")
-            report_file.write(f"Delivery ID: {delivery.ID}, Area: {delivery.area}, Priority: {delivery.priority}, Weight: {delivery.weight} kg\n\n")
+            report_file.write(
+                f"Delivery ID: {delivery.ID}, Area: {delivery.area}, "
+                f"Priority: {delivery.priority}, Weight: {delivery.weight} kg\n\n"
+            )
             report_file.write(f"The Reason for invalidation: {reason}\n")
             report_file.write("========================================\n\n")
     print("Report generated: invalid_deliveries_report.txt")
